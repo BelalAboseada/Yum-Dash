38805a0dc94de34626d34f032811d834ccfb77af
BelalAboSeada <belalaboseada@gmail.com>
2024-02-10 19:18:12 +0200

    Deploy Front

--- Backend/README.md
+++
@@ -1,2 +0,0 @@
-# Restaurant-application
-Restaurant application
--- Backend/items/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Items, Category
-
-# Register your models here.
-
-admin.site.register(Items)
-admin.site.register(Category)
